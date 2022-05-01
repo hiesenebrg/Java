56accb2c26339b97b3346e5d198fc0193c02f289
nasir-buddy <syedali172000@gmail.com>
2022-05-01 12:34:44 +0530

    pattern 22 0 and star

---
+++ Patterns/pat_22.java
@@ -0,0 +1,26 @@
+package Patterns;
+import java.util.Scanner;
+public class pat_22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of row : ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= 2*n+1; j++)
+            {
+                if(i == j  || j == n+1 || j == (2*(n+1)-i) )
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print("0");
+                }
+            }
+            {
+                System.out.println();
+            }
+        }
+    }
+}
